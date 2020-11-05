5e5b91c8371e909b301114660c29d3fe027d2d60
Lucas Lira <lucasrocha446@gmail.com>
2020-11-05 00:05:57 -0300

    formatting the code

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -11,20 +11,20 @@ export default function Pokemon({ pokemon }) {
 
 export async function getStaticProps({ params }) {
   const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
-  .then((respostaDoServer) => {
-    if (respostaDoServer.ok) {
-      return respostaDoServer.json();
-    }
+    .then((respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        return respostaDoServer.json();
+      }
 
-    throw new Error('Deu problema');
-  })
-  .then((respostaEmObjeto) => respostaEmObjeto);
+      throw new Error('Deu problema');
+    })
+    .then((respostaEmObjeto) => respostaEmObjeto);
 
   return {
     props: {
-      pokemon
-    }
-  }
+      pokemon,
+    },
+  };
 }
 
 export async function getStaticPaths() {
